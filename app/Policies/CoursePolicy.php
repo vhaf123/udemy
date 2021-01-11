3372cca32fc146bcdddd76164b9ff2a8ea013ec8
vhaf123 <victor.aranaf92@gmail.com>
2021-01-11 01:14:23 -0500

    aprovar curso

--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -39,4 +39,12 @@ class CoursePolicy
             return false;
         }
     }
+
+    public function revision(User $user, Course $course){
+        if($course->status == 2){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
