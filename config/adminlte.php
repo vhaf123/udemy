f0b2ead37dd67dc8f07d858e280f6f274625f967
vhaf123 <victor.aranaf92@gmail.com>
2021-01-16 22:05:27 -0500

    crud niveles

--- config/adminlte.php
+++ config/adminlte.php
@@ -270,6 +270,13 @@ return [
             'route'  => 'admin.categories.index',
             'icon' => 'fas fa-fw fa-cogs',
         ],
+
+        [
+            'text' => 'Niveles',
+            'route'  => 'admin.levels.index',
+            'icon' => 'fas fa-fw fa-chart-line',
+        ],
+
         [
             'text' => 'Pendientes de aprobación',
             'route'  => 'admin.courses.index',
